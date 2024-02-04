f9e2a53c584bb3b9a8a112ace59cb8ea4d3b1831
uuyhd <uuyhd@student.kit.edu>
2024-02-04 15:34:52 +0100

    Initial Commit

---
+++ SecretCameraRoll/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SecretCameraRoll
+//
+//  Created by Yves Nill on 04.02.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SecretCameraRoll/SecretCameraRollApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecretCameraRollApp.swift
+//  SecretCameraRoll
+//
+//  Created by Yves Nill on 04.02.24.
+//
+
+import SwiftUI
+
+@main
+struct SecretCameraRollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
